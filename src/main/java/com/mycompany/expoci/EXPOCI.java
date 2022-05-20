fd9470ba2d0f5a0be31c74a553272ba659209951
RubenTorre <105616669+RubenTorre@users.noreply.github.com>
2022-05-20 17:27:25 -0500

    agragando la funcion

--- src/main/java/com/mycompany/expoci/EXPOCI.java
+++ src/main/java/com/mycompany/expoci/EXPOCI.java
@@ -9,10 +9,22 @@ package com.mycompany.expoci;
  *
  * @author LENOVO
  */
+   import java.util.Scanner;
 public class EXPOCI {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-    
+
+
+  
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Introduce un radio");
+        double radio=sc.nextDouble();
+  
+        //Formula area circulo, usamos algunos de los metodos de Math
+        double area=Math.PI*Math.pow(radio, 2);
+  
+        System.out.println("El area del circulo es "+area);
+ 
     }
 }
